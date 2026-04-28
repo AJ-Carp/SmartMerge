9e571ae01555ac479f65126aa1e3c59838193905
AJ Carpinello <ajcarp81003@gmail.com>
2026-04-28 16:48:52 -0400

    project scaffold

---
+++ backend/src/main/java/com/smartmerge/SmartmergeApplication.java
@@ -0,0 +1,13 @@
+package com.smartmerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmergeApplication.class, args);
+	}
+
+}
